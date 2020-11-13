9a5704c77972a6f7a7cfa690edc23995e521527a
Guillaume PRIGENT <guillaume.prigent92@gmail.com>
2020-11-13 10:04:17 +0100

    logout . login update

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -4,7 +4,6 @@ const CLIENT_ID = "cccede7e89b5bc6";
 
 export default {
   login() {
-    console.log("Passé dans api/imgur");
     window.location = `https://api.imgur.com/oauth2/authorize?client_id=${CLIENT_ID}&response_type=token`;
   }
 };
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,23 +2,50 @@
   <div>
     <nav class="nav">
       <div class="nav--item"><router-link :to="{ name: 'Index' }" class="nav--item__link">Home</router-link></div>
-      <div class="nav--item"><router-link :to="{ name: 'Login' }" class="nav--item__link">Login</router-link></div>
       <div class="nav--item"><router-link :to="{ name: 'Favs' }" class="nav--item__link">Favs</router-link></div>
+
+      <div class="nav--item" v-if="imgur_token === null"><router-link :to="{ name: 'Login' }" class="nav--item__link">Login</router-link></div>
+      <div class="nav--item" v-else><div class="nav--item__link" v-on:click="logout">Logout</div></div>
     </nav>
   </div>
 </template>
 <script>
-import router from "vue-router";
+// import router from "vue-router";
+
+function getUrlVars() {
+  var vars = {};
+  window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+    vars[key] = value;
+  });
+  return vars;
+}
+
 export default {
   name: "Navbar",
+  data() {
+    return {
+      imgur_token: null
+    };
+  },
   methods: {
-    logout: () => {
+    logout() {
       localStorage.removeItem("imgur_token");
-      router.push({ name: "Index" });
+      this.imgur_token = null;
+      // document.location.href = "http://localhost:8081/#/";
     }
   },
-  data() {
-    return {};
+  beforeMount() {
+    if (localStorage.getItem("imgur_token") === null) {
+      this.imgur_token = localStorage.getItem("imgur_token");
+    }
+    return false;
+  },
+  created() {
+    let token = getUrlVars()["access_token"];
+    if (token) {
+      window.localStorage.setItem("imgur_token", token);
+      this.imgur_token = token;
+    }
   }
 };
 </script>
